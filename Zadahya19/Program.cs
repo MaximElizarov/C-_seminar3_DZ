1df20e5b0eebecd63d3e71638afb85ec62b8bc79
Maxim Elizmarov <kiro.dota322@gmail.com>
2022-11-30 11:34:24 +0300

    add DZ

---
+++ Zadahya19/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+Console.Clear();
+Console.Write("Ведите пятизначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int rev =0;
+int last = 0;
+int n1 = n;
+while (n1 > 0)
+{
+    last = n1 % 10;
+    rev = rev * 10 + last;
+    n1 = n1 / 10;
+}
+if (n == rev)
+    Console.Write("Yes");
+else
+    Console.Write("No");
+
+
+
+
+
+
+
+// Задача 19
+
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
---
+++ zadahya23/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Ведите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int k = 1;
+while (k <= n)
+{
+    Console.WriteLine(Math.Pow(k, 3));
+    k++;
+}
+
+
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
